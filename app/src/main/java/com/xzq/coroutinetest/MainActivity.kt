2cafe52cb04e704fe6db216cf04c21ec7b7754d8
xiazhiqiang <xiazhiqiang@bigo.sg>
2020-10-19 14:24:58 +0800

    coroutine

--- app/src/main/java/com/xzq/coroutinetest/MainActivity.kt
+++ app/src/main/java/com/xzq/coroutinetest/MainActivity.kt
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.xzq.coroutinetest.ui.main.MainFragment
 
+/**
+ * https://www.jianshu.com/p/76d2f47b900d
+ * https://juejin.im/post/6884979459196190728/
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commitNow()
         }
     }
 }
